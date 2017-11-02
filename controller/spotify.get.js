b757272fd367c9bcfa425bd0eedfa84bd8ab5e78
Adrien <adriendesola@gmail.com>
2017-11-02 14:47:29 +0100

    [FIX] refresh spotify token every 30 minutes

--- controller/spotify.get.js
+++ controller/spotify.get.js
@@ -4,13 +4,13 @@ module.exports = (req, res, next) => {
     shared
         .player
         .authorizationCodeGrant(req.query.code)
-        .then(({body}) => {
-            const {access_token, refresh_token} = body;
-
+        .then(({body: {access_token, refresh_token}}) => {
+            
             shared.player.setAccessToken(access_token);
             shared.player.setRefreshToken(refresh_token);
-            gladys.param.setValue({name: 'spotify_access_token', value: access_token});
+            
             gladys.param.setValue({name: 'spotify_refresh_token', value: refresh_token});
+
             res.status(200).json({message: 'Token successful added'});
         })
         .catch(error => {
